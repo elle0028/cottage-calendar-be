c3812bd7c07ec7ab14ba6a84b7eba6521e8f823c
mellero <m_ellero@hotmail.com>
2022-02-19 16:51:18 -0500

    Initial Data Models (Date, Note), plus migration

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# User is just AbstractUser
+
+class Date(models.Model):
+    date = models.DateTimeField()
+    users = models.ManyToManyField(User)
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.ForeignKey(Date, on_delete=models.CASCADE)
+    message = models.CharField(max_length=256)
