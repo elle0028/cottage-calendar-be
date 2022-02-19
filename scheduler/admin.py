d33f4501b00584a68cb10f21edeef4dc2bce1ec3
mellero <m_ellero@hotmail.com>
2022-02-19 17:50:35 -0500

    Change Date model to use YYYY-MM-DD format instead of datetime. Migration.

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Date
+from .models import Note
+
+admin.site.register(Date)
+admin.site.register(Note)
--- scheduler/models.py
+++ scheduler/models.py
@@ -1,13 +1,26 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import User
 
 # User is just AbstractUser
 
 class Date(models.Model):
-    date = models.DateTimeField()
+    date = models.CharField(unique=True,
+                            max_length=10,
+                            validators=[
+                                # Needs validation before insert to remove invalid dates
+                                RegexValidator(r'^20[2-9][0-9]-[0-1][0-9]-[0-3][0-9]$',
+                                                message='Date must be YYYY-MM-DD, after 2020.'
+                                )
+                            ])
     users = models.ManyToManyField(User)
+    def __str__(self):
+        return str(self.date) + '[' + ','.join(str(user) for user in self.users.all()) + ']'
 
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.ForeignKey(Date, on_delete=models.CASCADE)
     message = models.CharField(max_length=256)
+
+    def __str__(self):
+        return str(self.date) + ' -> ' + '"' + str(self.message) + '"' 
